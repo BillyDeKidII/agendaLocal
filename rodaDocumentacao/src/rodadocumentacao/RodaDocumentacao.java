e85b58b5fdef307c7b3a500710787879602e0f6a
Thiago Romão <thiaroll@gmail.com>
2019-08-24 21:29:55 -0300

    AGORA TA CERTO CARAI

--- rodaDocumentacao/src/rodadocumentacao/RodaDocumentacao.java
+++ rodaDocumentacao/src/rodadocumentacao/RodaDocumentacao.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class RodaDocumentacao {
@@ -29,28 +30,62 @@ public class RodaDocumentacao {
                     } else {
                         FileReader leitorArquivo = new FileReader(arquivo);
                         BufferedReader caminhadorArquivo = new BufferedReader(leitorArquivo);
-                        while (caminhadorArquivo.ready())
-                        {
+                        while (caminhadorArquivo.ready()) {
                             String linha = caminhadorArquivo.readLine();
                         }
                     }
                     File[] arquivos = arquivo.listFiles();
-                    FileWriter leitor = new FileWriter(arquivo, true);
-                    BufferedWriter escrita = new BufferedWriter(leitor);
+                    FileWriter leitor1 = new FileWriter(arquivo, true);
+                    BufferedWriter escrita1 = new BufferedWriter(leitor1);
                     Scanner leituraCase1 = new Scanner(System.in);
                     System.out.printf("Diga seu nome \n");
                     String nome = leituraCase1.nextLine();
                     System.out.printf("Diga seu numero \n");
                     String numero = leituraCase1.nextLine();
-                    escrita.write(nome);
-                    escrita.write(",");
-                    escrita.write(numero);
-                    escrita.newLine();
-                    escrita.close();
-                    leitor.close();
+                    escrita1.write(nome);
+                    escrita1.write(",");
+                    escrita1.write(numero);
+                    escrita1.newLine();
+                    escrita1.close();
+                    leitor1.close();
                     break;
                 case 2:
-                    System.out.println("Ainda não implementado! Escolha outra opção!");
+                    Scanner leituraExcluir = new Scanner(System.in);
+                    HashMap<String, String> listaExcluido = new HashMap<>();
+                    FileWriter leitor2 = new FileWriter(arquivo, true);
+                    BufferedWriter escrita2 = new BufferedWriter(leitor2);
+                    if (!arquivo.exists()) {
+
+                        System.out.println("Arquivo não existe meu companheiro! Execute a função 1 para gera um arquivo!");
+                    } else {
+                        FileReader leitorArquivo = new FileReader(arquivo);
+                        BufferedReader caminhadorArquivo = new BufferedReader(leitorArquivo);
+                        while (caminhadorArquivo.ready()) {
+                            String linha = caminhadorArquivo.readLine();
+                            String[] textoSeparado = linha.split(",");
+                            listaExcluido.put(textoSeparado[0], textoSeparado[1]);
+                        }
+                    }
+                    arquivo.delete();
+                    System.out.println("Digite o nome para ser excluido \n");
+                    String excluido = leituraExcluir.nextLine();
+                    listaExcluido.remove(excluido);
+                    for (String i : listaExcluido.keySet()) {
+                        if (!arquivo.exists()) {
+                            arquivo.createNewFile();
+                            escrita2.write(i);
+                            escrita2.write(",");
+                            escrita2.write(listaExcluido.get(i));
+                            escrita2.newLine();
+                        } else {
+                            escrita2.write(i);
+                            escrita2.write(",");
+                            escrita2.write(listaExcluido.get(i));
+                            escrita2.newLine();
+                        }
+                    }
+                    escrita2.close();
+                    leitor2.close();
                     break;
                 case 3:
                     System.out.println("Ainda não implementado! Escolha outra opção!");
@@ -74,4 +109,5 @@ public class RodaDocumentacao {
         } while (escolha != 0);
 
     }
+
 }
